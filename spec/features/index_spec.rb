497d913c2925ee524ee22d8315afbbc5af0b459c
lollypop27 <lara@adafctory.co.za>
2016-07-07 21:22:16 +0200

    adding header and footer partials

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,14 +1,29 @@
 require 'spec_helper'
 
-  describe 'index' do, type: :feature do
+  describe 'index', type: :feature do
     before do
       visit '/'
-    end
+  end
 
-    it 'says Hello Lara with a h1 tag' do
+  it 'says Welcome to Lara with a h1 tag' do
     expect(page).to have_selector 'h1'
     within 'h1' do
-      expect(page).to have_content 'Hello Lara'
+      expect(page).to have_content 'Welcome to Lara'
     end
   end
+
+  # it 'renders header partial' do
+  #   expect(page).to have_selector 'header'
+  #   within 'header' do
+  #     expect(page).to have_content 'header'
+  #   end
+  # end
+
+  it 'renders footer partial' do
+  expect(page).to have_selector 'footer'
+  within 'footer' do
+    expect(page).to have_content 'Site Map'
+    expect(page).to have_content 'Contact Me'
+  end
+end
 end
