a07c7e0c1d6f1fedff881f150b04c84547126c7f
lollypop27 <lara@adafctory.co.za>
2016-07-07 18:55:37 +0200

    adding css and js into header tag

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
-describe 'index' do, type: :feature do
-  before do
-    visit '/'
-  end
-  it "says hello Lara has h1 header" do
+  describe 'index' do, type: :feature do
+    before do
+      visit '/'
+    end
+
+    it 'says Hello Lara with a h1 tag' do
     expect(page).to have_selector 'h1'
     within 'h1' do
-      expect(page).to have_content 'Hello' lara
+      expect(page).to have_content 'Hello Lara'
     end
   end
 end
