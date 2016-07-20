c8f0957ce47e2362e0e66533c48ea6728dfdb9f1
lollypop27 <lara@adfactory.co.za>
2016-07-20 20:58:03 +0200

    updating test code to work spec files

--- spec/features/portfolio_spec.rb
+++ spec/features/portfolio_spec.rb
@@ -9,4 +9,14 @@ require 'spec_helper'
     expect(page).to have_selector('h1', text: 'My World of Coding and Design')
   end
 
+  it 'displays My First Website on the page' do
+    visit '/portfolio.html'
+    expect(page).to have_content 'My First Website'
+  end
+
+  it 'displays ATM Challenge on the page' do
+    visit '/portfolio.html'
+    expect(page).to have_content 'ATM Challenge'
+  end
+
 end
