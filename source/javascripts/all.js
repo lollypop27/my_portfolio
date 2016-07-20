ceabe05f07e6ef028fd3657d4ca194c76a58d120
lollypop27 <lara@adfactory.co.za>
2016-07-20 23:12:36 +0200

    adding contact page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -34,3 +34,40 @@ function fixedFooter() {
   $(window).resize(function () {
     fixedFooter(); //at page resize
   });
+
+  (function() {
+
+  	// Create input element for testing
+  	var inputs = document.createElement('input');
+
+  	// Create the supports object
+  	var supports = {};
+
+  	supports.autofocus   = 'autofocus' in inputs;
+  	supports.required    = 'required' in inputs;
+  	supports.placeholder = 'placeholder' in inputs;
+
+  	// Fallback for autofocus attribute
+  	if(!supports.autofocus) {
+
+  	}
+
+  	// Fallback for required attribute
+  	if(!supports.required) {
+
+  	}
+
+  	// Fallback for placeholder attribute
+  	if(!supports.placeholder) {
+
+  	}
+
+  	// Change text inside send button on submit
+  	var send = document.getElementById('contact-submit');
+  	if(send) {
+  		send.onclick = function () {
+  			this.innerHTML = '...Sending';
+  		}
+  	}
+
+  })();
