9959081a89d453cb621193c60c40253233f5ede0
lollypop27 <lara@adfactory.co.za>
2016-07-23 13:03:13 +0200

    adding content and styles to about page

--- spec/features/about_spec.rb
+++ spec/features/about_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
   describe 'about', type: :feature do
     before do
-      visit '/about'
+      visit '/about.html'
   end
+
+  it 'displays the correct heading' do
+    expect(page).to have_selector('h1', text: 'Welcome to My World of Design and Code')
+  end
+
+  it 'displays content' do
+    expect(page).to have_selector('p', text: 'Business owner, designer & part-time DJ who loves this changing world')
+  end
+
 end
